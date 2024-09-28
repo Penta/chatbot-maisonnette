a97fd038febaa0e3ff1f4000bf8fc517bf020a50
Andy Esnard <andy.esnard@gmail.com>
2024-09-28 23:36:47 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# Point d'entrée principal pour démarrer le bot Discord.
-
-from bot import client_discord
-from config import DISCORD_TOKEN
-
-# Démarrer le bot Discord
-client_discord.run(DISCORD_TOKEN)
